faecb5c9dc403a60df3e8b21b1bf22d9fd77bc65
Bell <bell@bell-MS-7758.(none)>
2013-02-07 11:27:29 +0400

    fixed scrape indexes

--- procurementScrape/spiders/procurement_spider.py
+++ procurementScrape/spiders/procurement_spider.py
@@ -530,7 +530,7 @@ class ProcurementSpider(BaseSpider):
                         break
         print "Starting scrape"
         url = self.mainPageBaseUrl+str(1)
-        metadata = {"page": 500, "final_page": int(600), "prevScrapeStartTender": lastTenderURL}
+        metadata = {"page": 1, "final_page": int(final_page), "prevScrapeStartTender": lastTenderURL}
         request = Request(url, errback=self.urlPageFailed,callback=self.parseTenderUrls, meta = metadata, cookies={"SPALITE":self.sessionCookie}, headers={"User-Agent":self.userAgent})
         self.firstTender = lastTenderURL
         
