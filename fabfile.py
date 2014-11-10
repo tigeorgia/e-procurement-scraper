0cc5fd8d6e19243ef7727c69274fd622473cf442
Etienne Baque <etiennebaque@gmail.com>
2014-11-10 13:57:43 +0400

    Added main script that will be called from Jenkins

---
+++ fabfile.py
@@ -0,0 +1,27 @@
+from fabric.api import run, env
+from DBImportExport import DBImportExport
+
+def update_db():
+    jobs = DBImportExport()
+    jobs.dumpdb()
+    jobs.compressdb()
+    jobs.uploaddb()
+    jobs.importdb()
+    jobs.cleanup()
+    jobs.postProcess()
+
+def import_online_db():
+    jobs = DBImportExport()
+    jobs.dump_procurement_db()
+    jobs.compress_online_db()
+    jobs.download_online_db()
+    jobs.import_db_scraper()
+    jobs.cleanup_online_archive()
+
+def post_scrape_process():
+    jobs = DBImportExport()
+    jobs.postProcess()
+
+def pre_scrape_process():
+    jobs = DBImportExport()
+    jobs.storePreScrapeSearchResults()
