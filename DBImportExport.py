7852d882caea41a7beb2e6ad087370e8926ca180
Marek Suliga <marek.suliga@transparency.ge>
2015-07-17 10:54:27 +0400

    Trying to fix the job failure

--- DBImportExport.py
+++ DBImportExport.py
@@ -11,91 +11,92 @@ class DBImportExport(object):
         '''
         Constructor
         '''
-	proc_creds = ProcurementCredentials()
-	self.info = proc_creds.load_info()
-	
-	host = '{0}@{1}'.format(self.info['remote_user'],self.info['remote_host'])
-	password = self.info['remote_password']
-
-	env.host_string=host
-	env.passwords={host: password}
-
-	env.user=self.info['remote_user']
-	env.password=password
+    	proc_creds = ProcurementCredentials()
+    	self.info = proc_creds.load_info()
+    	
+    	host = '{0}@{1}'.format(self.info['remote_user'],self.info['remote_host'])
+    	password = self.info['remote_password']
+    
+    	env.host_string=host
+    	env.passwords={host: password}
+    
+    	env.user=self.info['remote_user']
+    	env.password=password
 
 
 
     def dump_procurement_db(self):
-	print 'dump online procurement db.'
-
-	run('mysqldump -u {user} -p{dbpass} {db} --add-drop-table > {dbfile}'.format(user=self.info['remote_db_user'], db=self.info['remote_db'], dbpass=self.info['remote_db_pass'], dbfile=self.info['db_file_online']))
+    	print 'dump online procurement db.'
+    
+    	run('mysqldump -u {user} -p{dbpass} {db} --add-drop-table > {dbfile}'.format(user=self.info['remote_db_user'], db=self.info['remote_db'], dbpass=self.info['remote_db_pass'], dbfile=self.info['db_file_online']))
 
 
     # compress online db
     def compress_online_db(self):
-	print 'compresses online procurement db.'
-	run('tar czf {archivefile}.tar.gz {sqlfile}'.format(archivefile=self.info['db_file_online'],sqlfile=self.info['db_file_online']))
+    	print 'compresses online procurement db.'
+    	run('tar czf {archivefile}.tar.gz {sqlfile}'.format(archivefile=self.info['db_file_online'],sqlfile=self.info['db_file_online']))
 
 
     # download to scraper server
     def download_online_db(self):
-	print 'download online procurement db, uncompresses it, then deletes the local compressed file.'
-	get(self.info['db_zip_online'], self.info['db_zip_online'])
-	local('tar xzf {tarfile}'.format(tarfile=self.info['db_zip_online']))
-	local('rm {tarfile}'.format(tarfile=self.info['db_zip_online']))
+    	print 'download online procurement db, uncompresses it, then deletes the local compressed file.'
+    	get(self.info['db_zip_online'], self.info['db_zip_online'])
+    	local('tar xzf {tarfile}'.format(tarfile=self.info['db_zip_online']))
+    	local('rm {tarfile}'.format(tarfile=self.info['db_zip_online']))
 
 
     # import full db to scraper db
     def import_db_scraper(self):
-	print 'import downloaded procurement db into scraper db, then delete it.'
-	local('mysql -u {user} -p{dbpass} -D {db} < {dbfile}'.format(user=self.info['local_user'],db=self.info['local_db'], dbpass=self.info['local_db_pass'], dbfile=self.info['db_file_online']))
-	local('rm {sqlfile}'.format(sqlfile=self.info['db_file_online']))
+    	print 'import downloaded procurement db into scraper db, then delete it.'
+    	local('mysql -u {user} -p{dbpass} -D {db} < {dbfile}'.format(user=self.info['local_user'],db=self.info['local_db'], dbpass=self.info['local_db_pass'], dbfile=self.info['db_file_online']))
+    	local('rm {sqlfile}'.format(sqlfile=self.info['db_file_online']))
 
 
     def cleanup_online_archive(self):
-	print 'Deletes online files'
-	run('rm {sqlfile} {tarfile}.tar.gz'.format(sqlfile=self.info['db_file_online'],tarfile=self.info['db_file_online']))
+    	print 'Deletes online files'
+    	run('rm {sqlfile} {tarfile}.tar.gz'.format(sqlfile=self.info['db_file_online'],tarfile=self.info['db_file_online']))
 
 
     # Dump the database
     def dumpdb(self):
-	print 'Dump local procurement db.'
-	local('mysqldump -u {user} -p{dbpass} {db} --add-drop-table > {dbfile}'.format(user=self.info['local_user'],db=self.info['local_db'], dbpass=self.info['local_db_pass'], dbfile=self.info['db_file']))
+    	print 'Dump local procurement db.'
+    	local('mysqldump -u {user} -p{dbpass} {db} --add-drop-table > {dbfile}'.format(user=self.info['local_user'],db=self.info['local_db'], dbpass=self.info['local_db_pass'], dbfile=self.info['db_file']))
 
 
     def compressdb(self):
-	print 'Compresses the dumped db using tar.'
-	local('tar czf {tarfile}.tar.gz {sqlfile}'.format(tarfile=self.info['db_file'],sqlfile=self.info['db_file']))
+    	print 'Compresses the dumped db using tar.'
+    	local('tar czf {tarfile}.tar.gz {sqlfile}'.format(tarfile=self.info['db_file'],sqlfile=self.info['db_file']))
 
 
     def uploaddb(self):
-	print 'Uploads the compressed db file, uncompresses it remotely, and deletes the remote compressed file.'
-	put(self.info['db_zip'], self.info['db_zip'])
-	run('tar xzf {tarfile}'.format(tarfile=self.info['db_zip']))
-	run('rm {tarfile}'.format(tarfile=self.info['db_zip']))
+    	print 'Uploads the compressed db file, uncompresses it remotely, and deletes the remote compressed file.'
+    	put(self.info['db_zip'], self.info['db_zip'])
+    	run('tar xzf {tarfile}'.format(tarfile=self.info['db_zip']))
+    	run('rm {tarfile}'.format(tarfile=self.info['db_zip']))
 
 
     def importdb(self):
-	print 'Remotely imports the db file, then deletes it.'
-	run('ionice -c2 -n6 mysql -u {user} -p{dbpass} -D {db} < {dbfile}'.format(user=self.info['remote_db_user'],db=self.info['remote_db'], dbpass=self.info['remote_db_pass'], dbfile=self.info['db_file']))
-	run('rm {tarfile}'.format(tarfile=self.info['db_file']))
+    	print 'Remotely imports the db file, then deletes it.'
+    	run('ionice -c2 -n6 mysql -u {user} -p{dbpass} -D {db} < {dbfile}'.format(user=self.info['remote_db_user'],db=self.info['remote_db'], dbpass=self.info['remote_db_pass'], dbfile=self.info['db_file']))
+    	run('rm {tarfile}'.format(tarfile=self.info['db_file']))
 
 
     def storePreScrapeSearchResults(self):
-	with cd(self.info['remote_app_dir'] + '/current'):
-	    with shell_env(PATH=self.info['remote_app_dir'] + '/bin:$PATH',GEM_HOME=self.info['remote_app_dir'] + '/gems',RUBYLIB=self.info['remote_app_dir'] + '/lib'):
-		run('rake procurement:pre_store_search_results')
+    	with cd(self.info['remote_app_dir'] + '/current'):
+    	    with shell_env(PATH=self.info['remote_app_dir'] + '/bin:$PATH',GEM_HOME=self.info['remote_app_dir'] + '/gems',RUBYLIB=self.info['remote_app_dir'] + '/lib'):
+    		run('rake procurement:pre_store_search_results')
 
 
     def postProcess(self):
-	print 'Generates e-mail alerts and creates the CSV file'
-	with cd(self.info['remote_app_dir'] + '/current'):
-	    with shell_env(PATH=self.info['remote_app_dir'] + '/bin:$PATH',GEM_HOME=self.info['remote_app_dir'] + '/gems',RUBYLIB=self.info['remote_app_dir'] + '/lib'):
-		run('rake procurement:generate_alerts')
-		run('rake procurement:generate_tender_bulk_data')
-		run('zip ./public/AllTenders AllTenders.csv')
-		run('rake procurement:generate_procurement_csv')
-                run("zip ./public/AllSimplifiedProcurements AllSimplifiedProcurements_*.csv")
+        pass
+        # 	print 'Generates e-mail alerts and creates the CSV file'
+        # 	with cd(self.info['remote_app_dir'] + '/current'):
+        # 	    with shell_env(PATH=self.info['remote_app_dir'] + '/bin:$PATH',GEM_HOME=self.info['remote_app_dir'] + '/gems',RUBYLIB=self.info['remote_app_dir'] + '/lib'):
+        # 		run('rake procurement:generate_alerts')
+        # 		run('rake procurement:generate_tender_bulk_data')
+        # 		run('zip ./public/AllTenders AllTenders.csv')
+        # 		run('rake procurement:generate_procurement_csv')
+        #                 run("zip ./public/AllSimplifiedProcurements AllSimplifiedProcurements_*.csv")
 
     def generateFiles(self):
         print 'Creates the CSV file'
@@ -108,7 +109,7 @@ class DBImportExport(object):
 
 
     def cleanup(self):
-	print 'Cleans up the local dump file and tar file'
-	local('rm {sqlfile} {tarfile}.tar.gz'.format(sqlfile=self.info['db_file'],tarfile=self.info['db_file']))
+    	print 'Cleans up the local dump file and tar file'
+    	local('rm {sqlfile} {tarfile}.tar.gz'.format(sqlfile=self.info['db_file'],tarfile=self.info['db_file']))
 
 
